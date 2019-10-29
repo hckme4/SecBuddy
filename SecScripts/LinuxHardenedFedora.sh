9cd8938ad86869447f432a2841b76bb7c17a4636
hckme4 <hckme4@protonmail.com>
2019-10-29 13:45:12 -0400

    Completed Linux Hardened Fedora install script. still need to test these scripts to make sure they work as expected. 	modified:   LinuxHardenedFedora.sh

--- SecScripts/LinuxHardenedFedora.sh
+++ SecScripts/LinuxHardenedFedora.sh
@@ -2,19 +2,69 @@
 
 # SecBuddy Linux Hardened Kernel Install Script: Fedora Linux
 
+
 printf "Linux Hardened Kernel Installation: Fedora Linux"
 printf "WARNING! Installing a new kernel can break compatibility with software. A backup is recommended before proceeding!"
-printf "The linux-hardened kernel enables several security features. Would you like to install it now? [Y/n]: "
+printf "The linux-hardened kernel enables several security features. The installation time is over 1 hour as we are compiling it."
+printf "Would you like to install the hardened kernel now? [Y/n]: "
 
 read -r RUN_INSTALL
+
+CONFIG= uname -r
+CONFPATH=/boot/$CONFIG
 
 while true
 do
 	if ["$RUN_INSTALL" == "Y"]
 	then
 		# Because user inputted y we begin linux-hardened install
+		
+		# Clone a version of the linux4.9, with grsec patch added
+		git clone https://github.com/hckme4/linux-grsecurity.git
+		cd linux-grsecurity
 
-		# install code here
+		# copy old kernel config to Linux source tree to simplify installation
+		WORKINGDIR= pwd
+		cp $CONFPATH $WORKINGDIR/.config
+
+		# begin compiling hardened kernel. add -j MAKEOPT to speed up compilation.
+		make oldconfig
+		make bzImage
+		make modules
+		make modules_install
+		make install
+
+		printf "\nNew Kernel Installation Completed! Would you like to install additional security tools? [Y/n]: "
+		read -r ADDONS
+
+		while true
+		do
+			if [ "$ADDONS" == "Y" ]
+				#install firejail, usbguard and fail2ban
+				dnf install -y dnf-plugins-core
+				dnf copr enable heikoada/firejail
+				dnf install -y firejail usbguard fail2ban
+
+				#enable and start services
+				systemctl enable usbguard
+				systemctl enable fail2ban
+				systemctl start usbguard
+				systemctl start fail2ban
+				
+				#Exit
+				printf "Installation Finished, A reboot is recommended to use your new kernel.\n\n"
+				exit 0
+
+			elif [ "$ADDONS" == "n" ]
+				printf "Installation Finished, A reboot is recommended to use your new kernel.\n\n"
+				exit 0
+			else
+				printf "Enter a capital Y or a lowercase n: "
+				read -r ADDONS
+			fi
+		done
+		
+
 	elif ["$RUN_INSTALL" == "n"]
 	then
 		# Since user inputted n we notify user and quit the script.
